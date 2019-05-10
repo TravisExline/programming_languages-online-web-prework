a1fbdfd12a041d20d321d1a883d49bc3aa97ea58
Travis Exline <t.exline22@gmail.com>
2019-05-10 16:59:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,10 @@
-
-
 def reformat_languages(languages)
-  new_languages = {}
+  new_hash = {}
   languages.each do |type, hash|
     hash.each do |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
+      new_hash[name] ||= attributes
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << type
     end
   end
   new_languages
